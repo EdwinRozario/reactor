cd8ab38dc4631ee73092c8b23b944f35aa638ecb
edwinrozario <rozarioed@gmail.com>
2016-08-14 00:42:02 +1200

    Added Slider component'

---
+++ Slider.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+class Slider extends React.Component {
+	constructor(){
+		super();
+		this.state = { count: 0}
+		this.update = this.update.bind(this)
+	}
+
+	update(e){
+		this.setState({count: e.target.value})
+	}
+
+	render() {
+		return(
+			<div>
+				<h1>Rage is {this.state.count}.</h1>
+				<Widget update={this.update}/>
+				<p>This is a component with 1 prop and a widget</p>
+			</div>
+			);
+	}
+}
+
+class Widget extends React.Component {
+	render() {
+		return (
+				<input type='range'
+				 min='0'
+				 max='100'
+				 onChange={this.props.update}
+				/>
+			);
+	}
+}
+
+export default Slider
--- main.js
+++ main.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import FormFor from './FormFor';
+import Slider from './Slider';
 
 ReactDOM.render(
 	<App name="This is my first txt prop" age={10}/>, 
@@ -11,4 +12,9 @@ ReactDOM.render(
 ReactDOM.render(
 	<FormFor/>, 
 	document.getElementById('form'))
+;
+
+ReactDOM.render(
+	<Slider/>, 
+	document.getElementById('slider'))
 ;
